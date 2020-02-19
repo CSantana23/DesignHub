8469b520d9aed40628a9879b36adf37260167d9d
Mira Qiu <qmm0607@gmail.com>
2020-02-19 11:53:26 -0500

    First commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Design Hub</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -65,34 +65,32 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+{{--            @if (Route::has('login'))--}}
                 <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+{{--                    @auth--}}
+                        <a href="#">Home</a>
+{{--                    @else--}}
+                        <a href="#">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+{{--                        @if (Route::has('register'))--}}
+                            <a href="#">Register</a>
+{{--                        @endif--}}
+{{--                    @endauth--}}
                 </div>
-            @endif
+{{--            @endif--}}
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Design Hub
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/Mira-Qiu/Laravel-Shopping-platform/">GitHub</a>
                 </div>
             </div>
         </div>
